a017c02af82dfcebd4680c12cd24e8ad46406431
Nrxxx <quinteron557@gmail.com>
2025-09-03 15:55:12 -0500

    Changes to be committed: 	modified:   static/appTopara.js 	renamed:    templates/LANDING/x.html -> templates/LANDING/Ingresar.html 	modified:   templates/LANDING/topara.html 	deleted:    templates/LANDING/toparaPremium.html

--- static/appTopara.js
+++ static/appTopara.js
@@ -144,3 +144,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+// =========================
+// PANEL LATERAL (INGRESAR)
+// =========================
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+
+        const usernameInput = document.getElementById('username');
+        const passwordInput = document.getElementById('password');
+
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        if (username === 'admin' && password === '1234') {
+            alert('¡Inicio de sesión exitoso!')
+
+            window.location.href = 'topara.html';
+        } else {
+            alert('Usuario o contraseña incorrectos.');
+        }
+    });
+});
