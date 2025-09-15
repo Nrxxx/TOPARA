150db82cb7deee325342b8177baab5699b7e3cc2
Nrxxx <quinteron557@gmail.com>
2025-09-15 08:59:38 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   assets/img/1.webp
    	new file:   assets/img/2.webp
    	new file:   assets/img/3.webp
    	modified:   static/appTopara.js
    	modified:   static/styleTopara.css
    	modified:   templates/LANDING/topara.html

--- static/appTopara.js
+++ static/appTopara.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!carrucelItems.length) return;
 
   let currentIndex = 0;
-  const tiempoDeCambio = 10000;
+  const tiempoDeCambio = 5000;
   let automaticoInterval;
 
   function showSlide(index) {
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // =========================
-// MODAL
+// MODAL LOGIN
 // =========================
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("loginModal");
@@ -231,60 +231,60 @@ document.addEventListener('DOMContentLoaded', function() {
 // ------------------------------SIN TERMINAR --------------------------------------------------------------
 
 
-// const modal = document.getElementById("miModal");
-// const cerrarBtn = document.querySelector(".cerrar-btn");
-// const modalImagen = document.getElementById("modal-imagen");
-// const modalTitulo = document.getElementById("modal-titulo");
-// const modalDescripcion = document.getElementById("modal-descripcion");
+const modal = document.getElementById("miModal");
+const cerrarBtn = document.querySelector(".cerrar-btn");
+const modalImagen = document.getElementById("modal-imagen");
+const modalTitulo = document.getElementById("modal-titulo");
+const modalDescripcion = document.getElementById("modal-descripcion");
 
 
-// const productos = {
-//   producto1: {
-//     imagenSrc: "/img/TPRA410003-00N01_1.webp",
-//     titulo: "Producto de Muestra 1",
-//     descripcion: "Descripción detallada del primer producto. Aquí va toda la información relevante para el usuario."
-//   },
-//   producto2: {
-//     imagenSrc: "/img/TPRA410003-00N01_2.webp",
-//     titulo: "Producto de Muestra 2",
-//     descripcion: "Esta es la descripción del segundo producto. Explica sus características principales y beneficios."
-//   },
-//   producto3: {
-//     imagenSrc: "/img/TPRA410003-00N01_3.webp",
-//     titulo: "Producto de Muestra 3",
-//     descripcion: "Aquí se describe el tercer producto, con sus especificaciones y usos recomendados."
-//   }
-// };
+const productos = {
+  producto1: {
+    imagenSrc: "/img/TPRA410003-00N01_1.webp",
+    titulo: "Producto de Muestra 1",
+    descripcion: "Descripción detallada del primer producto. Aquí va toda la información relevante para el usuario."
+  },
+  producto2: {
+    imagenSrc: "/img/TPRA410003-00N01_2.webp",
+    titulo: "Producto de Muestra 2",
+    descripcion: "Esta es la descripción del segundo producto. Explica sus características principales y beneficios."
+  },
+  producto3: {
+    imagenSrc: "/img/TPRA410003-00N01_3.webp",
+    titulo: "Producto de Muestra 3",
+    descripcion: "Aquí se describe el tercer producto, con sus especificaciones y usos recomendados."
+  }
+};
 
 
-// function abrirModal(id) {
-//   const info = productos[id];
-//   if (info) {
-//     modalImagen.src = info.imagenSrc;
-//     modalTitulo.textContent = info.titulo;
-//     modalDescripcion.textContent = info.descripcion;
-//     modal.style.display = "block";
-//   }
-// }
+function abrirModal(id) {
+  const info = productos[id];
+  if (info) {
+    modalImagen.src = info.imagenSrc;
+    modalTitulo.textContent = info.titulo;
+    modalDescripcion.textContent = info.descripcion;
+    modal.style.display = "block";
+  }
+}
 
 
-// document.querySelectorAll(".imagen-producto").forEach(img => {
-//   img.addEventListener("click", (event) => {
-//     // Obtener el ID desde el atributo data-modal-id
-//     const id = event.target.getAttribute("data-modal-id");
-//     abrirModal(id);
-//   });
-// });
+document.querySelectorAll(".imagen-producto").forEach(img => {
+  img.addEventListener("click", (event) => {
+    // Obtener el ID desde el atributo data-modal-id
+    const id = event.target.getAttribute("data-modal-id");
+    abrirModal(id);
+  });
+});
 
 
-// cerrarBtn.addEventListener("click", () => {
-//   modal.style.display = "none";
-// });
+cerrarBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-// window.addEventListener("click", (event) => {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// });
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
 
 // --------------------------------------------------------------------------------------------
